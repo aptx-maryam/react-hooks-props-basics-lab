0948e45f80c139688fc55a7c08b7f46dce6c9ef7
Maryam <marym.ihnj@gmail.com>
2022-10-29 12:04:37 +0300

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 function Home(props) {
   return (
     <div id="home">
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <h1 style={{ color: props.color }}>
         {props.name} is a Web Developer from {props.city}
       </h1>
